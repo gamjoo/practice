193058805aae77df0c8333fffea700825b598fde
gamjoo <yyy970309@gmail.com>
2024-03-11 22:39:55 +0900

    240311 과제 마무리 후

--- JSP_Template_JSTL/src/main/java/net/template/action/TemplateFrontController.java
+++ JSP_Template_JSTL/src/main/java/net/template/action/TemplateFrontController.java
@@ -14,18 +14,26 @@ public class TemplateFrontController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	protected void doProcess(HttpServletRequest request,
-	HttpServletResponse response)
+//	protected void doProcess(HttpServletRequest request,
+//	HttpServletResponse response);
 	
 	//doProcess(request,response)메소드를 구현하여 요청이 GET방식이든
 	//POST방식으로 전송되어 오든 같은 메소드에서 요청을 처리할 수 있도록 작업
 	protected void doGet(HttpServletRequest request,
 	HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+//		doProcess(request, response);
+	String RequestURI = request.getRequestURI();
+	String ContextPath = request.getContextPath();
+	int ContextPathleng = ContextPath.length();
+	String command = RequestURI.substring(ContextPathleng);
+	System.out.println(ContextPathleng);
+	System.out.println("RequestURI = " + RequestURI);
+	System.out.println("contextPath = " + ContextPath);
+	System.out.println("command = " + command);
 	}
 	
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		doProcess(request, response);
+//		doProcess(request, response);
 	}
 }
